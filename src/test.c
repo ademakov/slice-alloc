a690340a5393770cfa6f74321c1ea56d261a6036
Aleksey Demakov <ademakov@gmail.com>
2020-12-25 14:21:12 +0300

    Extend test

--- src/test.c
+++ src/test.c
@@ -2,12 +2,46 @@
 #include "slice-alloc.h"
 
 #include <stdio.h>
+#include <pthread.h>
 
-int
-main(int ac, char **av)
+static void
+test(void)
 {
 	void *ptr = slice_alloc(100);
 	printf("%p\n", ptr);
 	slice_free(ptr);
+}
+
+static void *
+thread_test(void *arg)
+{
+	test();
+	return arg == NULL ? NULL : slice_alloc(100);
+}
+
+int
+main(int ac, char **av)
+{
+	test();
+
+	{
+		pthread_t tid;
+		pthread_create(&tid, NULL, thread_test, NULL);
+
+		void *ret;
+		pthread_join(tid, &ret);
+	}
+
+	{
+		pthread_t tid;
+		pthread_create(&tid, NULL, thread_test, (void *) 1);
+
+		void *ret;
+		pthread_join(tid, &ret);
+		slice_free(ret);
+
+//		slice_scrap_collect();
+	}
+
 	return 0;
 }
